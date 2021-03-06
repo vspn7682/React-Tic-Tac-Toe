c99c655f0bed4cf1683a39e822c2cd1cb7e64e43
Shailesh <vspn8569@gmail.com>
2021-03-06 16:26:48 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const itemsArray = new Array(9).fill('empty')
-const box = document.querySelector('.box')
+const itemsArray = new Array(9).fill('')
 
 const App = () => {
 
@@ -25,48 +24,43 @@ const App = () => {
   const reloadGame = () => {
     setIsCross(false)
     setWinMessage('')
-    itemsArray.fill('empty',0, 9)
+    itemsArray.fill('', 0,9)
   }
 
   const changeItem = (itemNumber) => {
     if (winMessage) {
-      return toast(winMessage, {type: "success"})
+      return toast(winMessage, {type: 'success'})
     }
 
-    if (itemsArray[itemNumber] === 'empty') {
-      itemsArray[itemNumber] = isCross ? 'cross' : 'circle'
+    if (itemsArray[itemNumber] === '') {
+      itemsArray[itemNumber] = isCross ? 'cross': 'circle' 
       setIsCross(!isCross)
     } else {
       return toast('Box already filled', {type: 'error'})
     }
 
-    checkWinner();
-  }
-
-  const changeBg = (itemNumber) => {
-    {itemsArray[itemNumber].style.background = 'red'}
+     checkWinner();
   }
 
   const checkWinner = () => {
-    if (itemsArray[0] === itemsArray[1] && itemsArray[0] === itemsArray[2] && itemsArray[0] !== 'empty') {
-      setWinMessage(`${itemsArray[0]} wins`)
-    } else if (itemsArray[3] === itemsArray[4] && itemsArray[3] === itemsArray[5] && itemsArray[3] !== 'empty') {
-      setWinMessage(`${itemsArray[3]} wins`)
-    } else if (itemsArray[6] === itemsArray[7] && itemsArray[6] === itemsArray[8] && itemsArray[6] !== 'empty') {
-      setWinMessage(`${itemsArray[6]} wins`)
-    } else if (itemsArray[0] === itemsArray[3] && itemsArray[0] === itemsArray[6] && itemsArray[0] !== 'empty') {
-      setWinMessage(`${itemsArray[0]} wins`)
-    } else if (itemsArray[1] === itemsArray[4] && itemsArray[1] === itemsArray[7] && itemsArray[1] !== 'empty') {
-      setWinMessage(`${itemsArray[1]} wins`)
-    } else if (itemsArray[2] === itemsArray[5] && itemsArray[2] === itemsArray[8] && itemsArray[2] !== 'empty') {
-      setWinMessage(`${itemsArray[2]} wins`)
-    } else if (itemsArray[0] === itemsArray[4] && itemsArray[0] === itemsArray[8] && itemsArray[0] !== 'empty') {
-      setWinMessage(`${itemsArray[0]} wins`)
-    } else if (itemsArray[2] === itemsArray[4] && itemsArray[2] === itemsArray[6] && itemsArray[2] !== 'empty') {
-      setWinMessage(`${itemsArray[2]} wins`)
-    } 
+          if (itemsArray[0] === itemsArray[1] && itemsArray[0] === itemsArray[2] && itemsArray[0] !== '') {
+            setWinMessage(`${itemsArray[0]} wins`)
+          } else if (itemsArray[3] === itemsArray[4] && itemsArray[3] === itemsArray[5] && itemsArray[3] !== '') {
+            setWinMessage(`${itemsArray[3]} wins`)
+          } else if (itemsArray[6] === itemsArray[7] && itemsArray[6] === itemsArray[8] && itemsArray[6] !== '') {
+            setWinMessage(`${itemsArray[6]} wins`)
+          } else if (itemsArray[0] === itemsArray[3] && itemsArray[0] === itemsArray[6] && itemsArray[0] !== '') {
+            setWinMessage(`${itemsArray[0]} wins`)
+          } else if (itemsArray[1] === itemsArray[4] && itemsArray[1] === itemsArray[7] && itemsArray[1] !== '') {
+            setWinMessage(`${itemsArray[1]} wins`)
+          } else if (itemsArray[2] === itemsArray[5] && itemsArray[2] === itemsArray[8] && itemsArray[2] !== '') {
+            setWinMessage(`${itemsArray[2]} wins`)
+          } else if (itemsArray[0] === itemsArray[4] && itemsArray[0] === itemsArray[8] && itemsArray[0] !== '') {
+            setWinMessage(`${itemsArray[0]} wins`)
+          } else if (itemsArray[2] === itemsArray[4] && itemsArray[2] === itemsArray[6] && itemsArray[2] !== '') {
+            setWinMessage(`${itemsArray[2]} wins`)
+          } 
   }
-
 
   return (
     <Container>
@@ -76,34 +70,125 @@ const App = () => {
         <Col md={6}>
           {winMessage ? (
             <div>
-              <h2 className="subheading">
-                {winMessage}
-              </h2>
-              <button onClick={reloadGame} className="btn">Reload</button>
+              <h2 className='subheading'>{winMessage}</h2>
+              <button onClick={reloadGame} className="btn">Reload Game</button>
             </div>
           ) : (
               <div>
-                <h2 className="subheading">
-                  {isCross ? 'Cross' : 'Circle'} turn
-                </h2>
+                <h2 className="subheading">{isCross ? 'Cross' : 'Circle'} turn</h2>
               </div>
           )}
           <div className="board">
             {itemsArray.map((item, index) => {
               return (
-                <Card onClick={()=>changeItem(index)}>
-                   <CardBody className="box">
-                      <Icon name={item}/>
-                   </CardBody>
-                </Card>
-             )
+                <Card onClick={()=> changeItem(index)}>
+                <CardBody className="box">
+                  <Icon name={item}/>
+                </CardBody>
+              </Card>
+              )
             })}
           </div>
         </Col>
       </Row>
     </Container>
   )
-
 }
 
 export default App;
+
+// const itemsArray = new Array(9).fill('empty')
+// const box = document.querySelector('.box')
+
+// const App = () => {
+
+//   //States
+//   const [isCross, setIsCross] = useState(false)
+//   const [winMessage, setWinMessage] = useState('')
+
+//   //Methods
+//   const reloadGame = () => {
+//     setIsCross(false)
+//     setWinMessage('')
+//     itemsArray.fill('empty',0, 9)
+//   }
+
+//   const changeItem = (itemNumber) => {
+//     if (winMessage) {
+//       return toast(winMessage, {type: "success"})
+//     }
+
+//     if (itemsArray[itemNumber] === 'empty') {
+//       itemsArray[itemNumber] = isCross ? 'cross' : 'circle'
+//       setIsCross(!isCross)
+//     } else {
+//       return toast('Box already filled', {type: 'error'})
+//     }
+
+//     checkWinner();
+//   }
+
+//   const changeBg = (itemNumber) => {
+//     {itemsArray[itemNumber].style.background = 'red'}
+//   }
+
+//   const checkWinner = () => {
+//     if (itemsArray[0] === itemsArray[1] && itemsArray[0] === itemsArray[2] && itemsArray[0] !== 'empty') {
+//       setWinMessage(`${itemsArray[0]} wins`)
+//     } else if (itemsArray[3] === itemsArray[4] && itemsArray[3] === itemsArray[5] && itemsArray[3] !== 'empty') {
+//       setWinMessage(`${itemsArray[3]} wins`)
+//     } else if (itemsArray[6] === itemsArray[7] && itemsArray[6] === itemsArray[8] && itemsArray[6] !== 'empty') {
+//       setWinMessage(`${itemsArray[6]} wins`)
+//     } else if (itemsArray[0] === itemsArray[3] && itemsArray[0] === itemsArray[6] && itemsArray[0] !== 'empty') {
+//       setWinMessage(`${itemsArray[0]} wins`)
+//     } else if (itemsArray[1] === itemsArray[4] && itemsArray[1] === itemsArray[7] && itemsArray[1] !== 'empty') {
+//       setWinMessage(`${itemsArray[1]} wins`)
+//     } else if (itemsArray[2] === itemsArray[5] && itemsArray[2] === itemsArray[8] && itemsArray[2] !== 'empty') {
+//       setWinMessage(`${itemsArray[2]} wins`)
+//     } else if (itemsArray[0] === itemsArray[4] && itemsArray[0] === itemsArray[8] && itemsArray[0] !== 'empty') {
+//       setWinMessage(`${itemsArray[0]} wins`)
+//     } else if (itemsArray[2] === itemsArray[4] && itemsArray[2] === itemsArray[6] && itemsArray[2] !== 'empty') {
+//       setWinMessage(`${itemsArray[2]} wins`)
+//     } 
+//   }
+
+
+//   return (
+//     <Container>
+//       <h1 className="heading">Tic Tac Toe</h1>
+//       <ToastContainer position="bottom-center"></ToastContainer>
+//       <Row>
+//         <Col md={6}>
+//           {winMessage ? (
+//             <div>
+//               <h2 className="subheading">
+//                 {winMessage}
+//               </h2>
+//               <button onClick={reloadGame} className="btn">Reload</button>
+//             </div>
+//           ) : (
+//               <div>
+//                 <h2 className="subheading">
+//                   {isCross ? 'Cross' : 'Circle'} turn
+//                 </h2>
+//               </div>
+//           )}
+//           <div className="board">
+//             {itemsArray.map((item, index) => {
+//               return (
+//                 <Card onClick={()=>changeItem(index)}>
+//                    <CardBody className="box">
+//                       <Icon name={item}/>
+//                    </CardBody>
+//                 </Card>
+//              )
+//             })}
+//           </div>
+//         </Col>
+//       </Row>
+//     </Container>
+//   )
+
+// }
+
+// export default App;
--- src/Components/Icon.js
+++ src/Components/Icon.js
@@ -1,17 +1,35 @@
 import React from 'react'
 
-import { FaTimes, FaPen, FaRegCircle } from 'react-icons/fa'
+//Icons import
+import { FaPen, FaTimes, FaRegCircle } from 'react-icons/fa'
 
-const Icon = ({ name }) => {
-    switch (name) {
-        case "cross":
-            return <FaTimes />
-        case "circle":
-            return <FaRegCircle />
-
-        case "empty":
-            return ""
+const Icons = ({ name }) => {
+    if (name === 'circle') {
+        return <FaRegCircle />
+    } else if (name === 'cross') {
+        return <FaTimes />
+    } else {
+        return <FaPen />
     }
 }
 
-export default Icon;
+export default Icons
+
+
+// import React from 'react'
+
+// import { FaTimes, FaPen, FaRegCircle } from 'react-icons/fa'
+
+// const Icon = ({ name }) => {
+//     switch (name) {
+//         case "cross":
+//             return <FaTimes />
+//         case "circle":
+//             return <FaRegCircle />
+
+//         case "empty":
+//             return ""
+//     }
+// }
+
+// export default Icon;
